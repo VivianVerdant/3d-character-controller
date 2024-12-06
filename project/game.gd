f45c73f025dae69e050b403e9ffa3a2a6741dce8
Vivian <68966813+VivianVerdant@users.noreply.github.com>
2024-12-05 19:09:14 -0600

    Initial commit

---
+++ project/game.gd
@@ -0,0 +1,15 @@
+extends Node
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("toggle_fullscreen"):
+		get_viewport().mode = (
+			Window.MODE_FULLSCREEN if
+			get_viewport().mode != Window.MODE_FULLSCREEN else
+			Window.MODE_WINDOWED
+		)
+
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	elif event.is_action_pressed("left_click"):
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
---
+++ start_editor.bat
@@ -0,0 +1,1 @@
+..\Godot_v4.4-dev6_win64.exe project/project.godot
